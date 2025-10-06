a5dd56a685190113fcecc354205130fcf9d5359a
accotech01 <accotech01@gmail.com>
2025-10-06 11:28:16 +0100

    added index.html, styles.css and quiz.js with the neccessary codes

---
+++ quiz.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    function checkAnswer() {
+        correctAnswer = "4"
+
+        const userAnswer = document.querySelector('input[name="quiz"]:checked').value
+
+        if (userAnswer == correctAnswer){
+            feedback = document.getElementById('feedback');
+            feedback.textContent = 'Correct! Well done.'
+        } else {
+            feedback.textContent = "That's incorrect. Try again!"
+        }
+
+        
+    }
+
+    submitAnswer = document.getElementById('submit-answer');
+
+        submitAnswer.addEventListener('click', checkAnswer)
+})
+
